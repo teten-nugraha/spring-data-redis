70bce6a22ee9ccf104358108518eecc3aca0dee5
Teten Nugraha <teten.nugraha18@gmail.com>
2022-01-25 09:20:03 +0700

    define entity

---
+++ src/main/java/id/ten/springdataredis/entity/Student.java
@@ -0,0 +1,48 @@
+package id.ten.springdataredis.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+import lombok.Data;
+
+@RedisHash(value = "student")
+public class Student {
+    @Id
+    @Indexed
+    private String id;
+    private String name;
+    private String grade;
+
+    public Student(String id, String name, String grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Student() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
