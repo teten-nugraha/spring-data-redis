5c3a1bc8c0abcceefba31a09c989d726d93bb738
Teten Nugraha <teten.nugraha18@gmail.com>
2022-01-25 08:39:45 +0700

    first project

---
+++ src/main/java/id/ten/springdataredis/SpringDataRedisApplication.java
@@ -0,0 +1,17 @@
+package id.ten.springdataredis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisApplication.class, args);
+    }
+
+
+}
